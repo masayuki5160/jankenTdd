8d96f918392612984d2647d2b6b32a5f78f5ad4f
masayuki tanaka <masayuki5160@gmail.com>
2017-12-26 21:54:10 +0900

    テストケース追加とテストケースのグルーピングを実施

--- src/JankenTest.java
+++ src/JankenTest.java
@@ -1,41 +1,72 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
 
+@RunWith(Enclosed.class)
 public class JankenTest {
 
-	@Test
-	public void 設定したルールが指定したようにセットされている() {
-		Game game = new Game(1);
-		game.start(new Rock());
-		assertEquals(1, game.gameCount);
+	public static class ジャンケンゲームの環境周りを確認{
+		@Test
+		public void 設定したルールが指定したようにセットされている() {
+			Game game = new Game(1);
+			game.start(new Rock());
+			assertEquals(1, game.gameCount);
+		}
 	}
 	
-	@Test
-	public void グーの時はチョキに勝つ() {
-		Game game = new Game(1);
-		game.start(new Rock());
-		GameResult currentGameResult = game.currentGameInfo(new Scissors());
-		
-		assertEquals(GameResult.JankenResult.Win, currentGameResult.Result);
-	}
 	
-	@Test
-	public void グーの時はパーに負ける() {
-		Game game = new Game(1);
-		game.start(new Rock());
-		GameResult currentGameResult = game.currentGameInfo(new Paper());
+	public static class パターンテスト{
 		
-		assertEquals(GameResult.JankenResult.Lose, currentGameResult.Result);
-	}
+		Game game;
+		
+		@Before
+		public void setUp() {
+			game = new Game(1);
+		}
 	
-	@Test
-	public void グーの時はグーとあいこになる() {
-		Game game = new Game(1);
-		game.start(new Rock());
-		GameResult currentGameResult = game.currentGameInfo(new Rock());
+		@Test
+		public void グーの時はチョキに勝つ() {
+			game.start(new Rock());
+			GameResult currentGameResult = game.currentGameInfo(new Scissors());
+			
+			assertEquals(GameResult.JankenResult.Win, currentGameResult.Result);
+		}
 		
-		assertEquals(GameResult.JankenResult.Draw, currentGameResult.Result);
+		@Test
+		public void グーの時はパーに負ける() {
+			game.start(new Rock());
+			GameResult currentGameResult = game.currentGameInfo(new Paper());
+			
+			assertEquals(GameResult.JankenResult.Lose, currentGameResult.Result);
+		}
+		
+		@Test
+		public void グーの時はグーとあいこになる() {
+			game.start(new Rock());
+			GameResult currentGameResult = game.currentGameInfo(new Rock());
+			
+			assertEquals(GameResult.JankenResult.Draw, currentGameResult.Result);
+		}
+		
+		@Test
+		public void パーの時はチョキに負ける() {
+			game.start(new Paper());
+			GameResult currentGameResult = game.currentGameInfo(new Scissors());
+			
+			assertEquals(GameResult.JankenResult.Lose, currentGameResult.Result);
+		}
+		
+		@Test
+		public void パーの時はグーに勝つ() {
+			game.start(new Paper());
+			GameResult currentGameResult = game.currentGameInfo(new Scissors());
+			
+			assertEquals(GameResult.JankenResult.Lose, currentGameResult.Result);
+		}
+	
 	}
 
 }
